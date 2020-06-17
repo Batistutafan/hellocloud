cb43a697f521ff704705889d22b3eccc869f74d9
yanbofan <616652962@qq.com>
2020-06-17 19:45:53 +0800

    从头开始学习

---
+++ src/test/java/com/cctv/hello/HelloApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cctv.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class HelloApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
